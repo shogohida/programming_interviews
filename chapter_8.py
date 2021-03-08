04f95bd0a72b12de254e1d2a13a7f8a8d17617b2
Shogo Hida <shogo.hida@gmail.com>
2021-03-08 22:23:36 +0900

    added stack file

---
+++ chapter_8.py
@@ -0,0 +1,23 @@
+class Stack:
+    ElementWithCachedMax = collections.namedtuple('ElementWithCachedMax', ('element', 'max'))
+
+    def __init__(self):
+        self._element_with_cached_max = []
+
+    def empty(self):
+        return len(self._element_with_cached_max) == 0
+
+    def max(self):
+        if self.empty():
+            raise IndexError('max(): empty stack')
+        return self._element_with_cached_max[-1].max
+
+    def pop(self):
+        if self.empty():
+            raise IndexError('pop(): empty stack')
+        return self._element_with_cached_max.pop().element
+
+    def push(self, x):
+        self._element_with_cached_max.append(
+            self.ElementWithCachedMax(x, x if self.empty() else max(
+                x, self.max())))
